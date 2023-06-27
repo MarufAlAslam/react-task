49f3859cd069f52e52273dd5219aded1b5e15b22
marufAlAslam <maruf14@cse.pstu.ac.bd>
2023-06-27 10:38:05 +0600

    sidebar done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './assets/css/style.css'
 import { RouterProvider } from 'react-router-dom';
 import router from './routes/main';
 
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
+import Sidebar from "../../components/sidebar";
 
 const Home = () => {
-    return (
-        <div>
-            Homepage
+  return (
+    <div>
+      <div className="flex justify-between items-center">
+        <div className="w-2/12">
+          <Sidebar />
         </div>
-    )
-}
+        <div className="w-10/12 p-2 bg-yellow-50"></div>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
